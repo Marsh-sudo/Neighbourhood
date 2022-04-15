54955c943bb931331b5d217fac9beed8daf360ac
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-15 03:23:00 +0300

    create models

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=150)
+    location = models.CharField(max_length=150)
+    admin = models.ForeignKey(User,on_delete=models.CASCADE)
+    logo =  models.ImageField(upload_to='images/')
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.TextField(max_length=250,blank=True)
+    name = models.CharField(max_length=250,blank=True)
+    profile_pic = models.ImageField(upload_to='images/')
+    neighbourhood = models.ForeignKey(Neighbourhood,null=True,blank=True)
+
+
