02f673da1c0f350be192e965059ed6f0f19e70ee
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-16 04:37:29 +0300

    create url to home

---
+++ hood/urls.py
@@ -0,0 +1,10 @@
+from django.urls.conf import re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns=[
+    re_path('^$',views.index,name='home'),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
