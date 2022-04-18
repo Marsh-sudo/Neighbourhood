cc80ba7e7fca9bbfaa50626b27f7767b5961b94d
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-18 05:15:49 +0300

    change repath to path

--- hood/urls.py
+++ hood/urls.py
@@ -1,4 +1,4 @@
-from django.urls.conf import re_path
+from django.urls.conf import re_path,path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -6,7 +6,9 @@ from . import views
 urlpatterns=[
     re_path('^$',views.index,name='home'),
     re_path('^hood$',views.hoods,name='estates'),
-    re_path('^my_estate/<int:id>$',views.estates,name='my_estate'),
+    path('my_estate/<int:id>',views.estates,name='my_estate'),
+    re_path('^newbusiness/<int:id>$',views.new_business,name='newbusiness'),
+    re_path('^profiles$',views.profile,name='profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
