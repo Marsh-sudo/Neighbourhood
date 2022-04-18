0381086a4afad60eda813837c70d77ecd9a1dda8
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-18 05:15:19 +0300

    add view function for new business

--- hood/views.py
+++ hood/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .models import Neighbourhood,Profile,Business
-
+from .forms import BusinessForm,UpdateUserForm,UpdateUserProfileForm
 
 # Create your views here.
 # @login_required(login_url='/accounts/register/')
@@ -16,7 +17,33 @@ def hoods(request):
 
 
 def estates(request,id):
-    post = get_object_or_404(Neighbourhood,pk=id)
+    post = Neighbourhood.objects.get(id=id)
+    # post = get_object_or_404(Neighbourhood,pk=id)
     # houses = Neighbourhood.objects.filter(hoods=id)
 
-    return render(request,'all-neighbor/hoods.html',{"houses":post})
+    return render(request,'all-neighbor/details.html',{"post":post})
+
+def new_business(request,id):
+    current_user = request.user
+    if request.method == "POST":
+        bus_form = BusinessForm(request.POST,request.FILES)
+        bus_form.instance.user = request.user
+        if bus_form.is_valid():
+            bus_form.save()
+            return redirect('my_estate',id)
+
+
+def profile(request):
+    profile = Profile.display_profile()
+    current_user = request.user
+
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST,instance=request.user)
+        profile_form = UpdateUserProfileForm(request.POST,request.FILES)
+        if profile_form.is_valid():
+            profile_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        profile_form = UpdateUserProfileForm()
+
+    return render (request,'all-neighbor/profile.html',{"profile_form":profile_form,"profiles":profile})
