ed1f127c651044705168104df04af14fea1cf406
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-19 11:56:15 +0300

    run tests

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Post,Business,Neighbourhood
 
 # Create your tests here.
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.kasarani=Neighbourhood(name='kasarani',description='best neighborhood',health_number='07123453234')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kasarani,Neighbourhood))
+
+    def test_create_method(self):
+        self.kasarani.save()
+        neighbors = Neighbourhood.objects.all()
+        self.assertTrue(len(neighbors))
